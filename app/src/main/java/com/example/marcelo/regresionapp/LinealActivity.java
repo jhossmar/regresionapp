2a9197927630b9f06cecc6b3cd803fb94a14845e
jhossmar <jhosmar_ramos@hotmail.com>
2018-11-14 02:35:13 -0400

    segunda actividad

--- app/src/main/java/com/example/marcelo/regresionapp/LinealActivity.java
+++ app/src/main/java/com/example/marcelo/regresionapp/LinealActivity.java
@@ -15,17 +15,9 @@ public class LinealActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lineal);
 
-        TextView textw_atras = findViewById(R.id.btn_atras);
 
-        final Button btn_atras = findViewById(R.id.btn_atras);
-        btn_atras.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    btn_atras.setText("SE HIZO CLICK EN ATRAS");
-                Intent intent = new Intent(LinealActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
     }
 
 }
