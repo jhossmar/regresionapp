d03b0d97b75c89ff1e58032be8d25e77cb2af6ce
jhossmar <jhosmar_ramos@hotmail.com>
2018-12-03 10:57:43 -0400

    conf test witch espresso

--- app/src/androidTest/java/com/example/marcelo/regresionapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/marcelo/regresionapp/ExampleInstrumentedTest.java
@@ -1,13 +1,24 @@
 package com.example.marcelo.regresionapp;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,12 +26,39 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+    private String mStringBeTyped;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void ValorInicial(){
+        mStringBeTyped = "Espresso";
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        onView(withText("MINIMOS CUADRADOS")).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_lineal)).perform(click());
 
-        assertEquals("com.example.marcelo.regresionapp", appContext.getPackageName());
-    }
+        onView(withId(R.id.editText_lineal_x))
+                .perform(typeText("18.9"), closeSoftKeyboard());
+        onView(withId(R.id.editText_lineal_y))
+                .perform(typeText("56.9"), closeSoftKeyboard());
+
+        onView(withId(R.id.add_dato))
+                .perform(click());
+
+        onView(withId(R.id.calcular))
+                .perform(click());
+
+        onView(withText("RESULTADOS")).check(matches(isDisplayed()));
+
+//        assertEquals("com.example.marcelo.regresionapp", appContext.getPackageName());
+   }
 }
