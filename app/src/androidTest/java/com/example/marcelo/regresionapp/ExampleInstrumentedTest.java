ea691628123b15ef74978859c6f6c933220a433c
marcelojhossmar <marcelojhossmar@gmail.com>
2018-12-07 12:25:08 -0400

    test edad_peso

--- app/src/androidTest/java/com/example/marcelo/regresionapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/marcelo/regresionapp/ExampleInstrumentedTest.java
@@ -18,6 +18,8 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.core.AllOf.allOf;
 
 
 /**
@@ -28,37 +30,29 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ExampleInstrumentedTest {
-    private String mStringBeTyped;
-
+    private  double[] x={12,8,10,11,7,7,10,14};
+    private  double[] y={58,42,51,54,40,39,49,56};
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
     @Before
     public void ValorInicial(){
-        mStringBeTyped = "Espresso";
     }
-
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-//        Context appContext = InstrumentationRegistry.getTargetContext();
-//
-//        onView(withText("MINIMOS CUADRADOS")).check(matches(isDisplayed()));
+    public void test_Edad_Peso() {
         onView(withId(R.id.btn_lineal)).perform(click());
 
-        onView(withId(R.id.editText_lineal_x))
-                .perform(typeText("18.9"), closeSoftKeyboard());
-        onView(withId(R.id.editText_lineal_y))
-                .perform(typeText("56.9"), closeSoftKeyboard());
-
-        onView(withId(R.id.add_dato))
-                .perform(click());
-
+        for (int i=0; i<x.length; i++){
+            onView(withId(R.id.editText_lineal_x))
+                    .perform(typeText(""+x[i]), closeSoftKeyboard());
+            onView(withId(R.id.editText_lineal_y))
+                    .perform(typeText(""+y[i]), closeSoftKeyboard());
+            onView(withId(R.id.add_dato))
+                    .perform(click());
+        }
         onView(withId(R.id.calcular))
                 .perform(click());
 
-        onView(withText("RESULTADOS")).check(matches(isDisplayed()));
-
-//        assertEquals("com.example.marcelo.regresionapp", appContext.getPackageName());
+        System.out.println(onView(withId(R.id.valor_A)));
+        onView(withId(R.id.valor_A)).check(matches(withText(containsString("20.6122"))));
    }
 }
